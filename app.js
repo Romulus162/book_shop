380f8b0a61f1fab5d9b6812e6d70331d86e69607
Roman <Roman.badiere@gmail.com>
2023-10-24 17:48:11 -0600

    api and client auth communication

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -27,7 +37,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kbdo7pq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(8000);
   })
   .catch(err => {
     console.log(err);
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,8 +1,96 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
+  state = {
+    isLogin: true,
+  };
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  switchMode = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let request = {
+      query: `
+        query {
+            login(email: "${email}", password: "${password}") {
+                staffId
+                token
+                tokenExpiration
+            }
+        }
+        `,
+    };
+
+    if (!this.state.isLogin) {
+      request = {
+        query: `
+            mutation {
+              createStaff(staffInput: {email: "${email}", password: "${password}"}){
+                _id
+                email
+              }
+            }
+              `,
+      };
+    }
+
+    fetch('http://localhost:8000/api', {
+      method: 'POST',
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return <h1>The Auth page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchMode}>
+            Go to {this.state.isLogin ? 'Signup' : 'Login'}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
