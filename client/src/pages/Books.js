9c189c7be7417549bb744d988c18f474e462b33b
Roman <Roman.badiere@gmail.com>
2023-10-26 21:45:41 -0600

    stopping point for now

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -9,15 +9,39 @@ class BooksPage extends Component {
     creating: false,
   };
 
+  constructor(props) {
+    super(props);
+    this.titleRef = React.createRef();
+    this.authorRef = React.createRef();
+    this.priceRef = React.createRef();
+    this.descriptionRef = React.createRef();
+  }
+
   createBookHandler = () => {
     this.setState({ creating: true });
   };
 
-  cancelHandler = () => {
+  confirmHandler = () => {
     this.setState({ creating: false });
+    const title = this.titleRef.current.value;
+    const author = this.authorRef.current.value;
+    const price = +this.priceRef.current.value;
+    const description = this.descriptionRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      author.trim().length === 0 ||
+      price.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+
+    const event = { title, author, price, description };
+    console.log(event);
   };
 
-  confirmHandler = () => {
+  cancelHandler = () => {
     this.setState({ creating: false });
   };
 
@@ -33,7 +57,28 @@ class BooksPage extends Component {
             onCancel={this.cancelHandler}
             onConfirm={this.confirmHandler}
           >
-            <p>modal content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="Title">Title</label>
+                <input type="text" id="title" ref={this.titleRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="Author">Author</label>
+                <input type="text" id="author" ref={this.authorRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="Price">Price</label>
+                <input type="number" id="price" ref={this.priceRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="Description">Description</label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  ref={this.descriptionRef}
+                ></textarea>
+              </div>
+            </form>
           </Modal>
         )}
         <div className="books-control">
