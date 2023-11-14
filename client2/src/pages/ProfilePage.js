0fa315171cd9372503769f076482ebf0ecce439f
Roman <Roman.badiere@gmail.com>
2023-11-13 23:44:12 -0700

    very basic usr profile page for backend stuff

--- client2/src/pages/ProfilePage.js
+++ client2/src/pages/ProfilePage.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const Profile = () => {};
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <h1>User Profile</h1>
+      <div className="profile-info">
+        <img src="profile-picture-url.jpg" alt="Profile" />
+        <h2>John Doe</h2>
+        <p>Email: meow@cat.com</p>
+        <p>About: blahblahblah</p>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
