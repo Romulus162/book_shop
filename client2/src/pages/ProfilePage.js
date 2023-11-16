7d02109098ae87203e9233a3a4c8735411d20d05
Roman <Roman.badiere@gmail.com>
2023-11-15 17:59:15 -0700

    non-functional edit button

--- client2/src/pages/ProfilePage.js
+++ client2/src/pages/ProfilePage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './ProfilePage.css';
 
 const Profile = () => {
+  const handleEditProfile = () => {
+    console.log('Edit Profile Clicked');
+  };
+
   return (
     <div className="profile-container">
       <h1>User Profile</h1>
@@ -10,6 +14,10 @@ const Profile = () => {
         <h2>John Doe</h2>
         <p>Email: meow@cat.com</p>
         <p>About: blahblahblah</p>
+
+        <button onClick={handleEditProfile} className="edit-profile-btn">
+          Edit Profile
+        </button>
       </div>
     </div>
   );
